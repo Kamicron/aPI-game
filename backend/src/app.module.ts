24c406f335986e66f14f39b129eeb60ee852f657
Ludovic Chevroulet <ludovic@valuemycar.fr>
2025-11-21 12:11:16 +0100

    feat(app): Integrate ChatModule into AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { AppService } from './app.service';
         synchronize: true,
       }),
     }),
+    ChatModule,
   ],
   controllers: [AppController],
   providers: [AppService],
